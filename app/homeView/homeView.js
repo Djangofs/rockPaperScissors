7bc0422f4f05a39344f2373ab4b67f7f51c1b7fc
Django Shelton <djangoshelton@gmail.com>
2016-11-25 10:12:04 +0000

    added resetScore and scoreCount functions

--- app/homeView/homeView.js
+++ app/homeView/homeView.js
@@ -3,4 +3,29 @@
 angular.module('rockPaperScissors')
   .controller('HomeViewCtrl', function($scope) {
 
+    $scope.playerScore = 0;
+    $scope.compScore = 0;
+
+    //Function to increment player scores
+    $scope.scoreCount = function(result) {
+
+      if (result === "Player") {
+
+        $scope.playerScore += 1;
+
+      } else if (result === "Comp") {
+
+        $scope.compScore += 1;
+
+      }
+    };
+
+    //Function to reset both players scores to zero
+    $scope.resetScore = function () {
+
+      $scope.playerScore = 0;
+      $scope.compScore = 0;
+
+    };
+
 });
--- app/homeView/homeView_test.js
+++ app/homeView/homeView_test.js
@@ -24,5 +24,72 @@ describe('Controller: HomeViewController', function() {
       expect(HomeViewCtrl).toBeDefined();
     });
 
+    it('should define player and computer score', function() {
+
+      //Assert
+      expect(scope.playerScore).toBeDefined();
+      expect(scope.compScore).toBeDefined();
+    });
+
+    it('should define scoreCount and resetScore functions', function() {
+
+      //Assert
+      expect(scope.scoreCount).toBeDefined();
+      expect(scope.resetScore).toBeDefined();
+    });
+  });
+
+  describe('Function: scoreCount', function() {
+
+    it('should increment player score when called with Player', function() {
+
+      //Arrange
+      scope.playerScore = 0;
+      scope.compScore = 0;
+      var result = "Player";
+
+      //Act
+      scope.scoreCount(result);
+
+      //Assert
+      expect(scope.playerScore).toBe(1);
+      expect(scope.compScore).toBe(0);
+
+    });
+
+    it('should increment player score when called with Comp', function() {
+
+      //Arrange
+      scope.playerScore = 0;
+      scope.compScore = 0;
+      var result = "Comp";
+
+      //Act
+      scope.scoreCount(result);
+
+      //Assert
+      expect(scope.playerScore).toBe(0);
+      expect(scope.compScore).toBe(1);
+
+    });
+  });
+
+  describe('Function: resetScore', function() {
+
+    it('should set playerScore and compScore to 0 when called', function() {
+
+      //Arrange
+      scope.playerScore = 20;
+      scope.compScore = 15;
+
+      //Act
+      scope.resetScore();
+
+      //Assert
+      expect(scope.playerScore).toBe(0);
+      expect(scope.compScore).toBe(0);
+
+    })
+
   });
 });
