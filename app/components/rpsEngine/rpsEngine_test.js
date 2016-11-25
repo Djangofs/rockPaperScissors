90baf96f023f99ec2a3f4aeb0fa3c4286b2b8c8f
Django Shelton <djangoshelton@gmail.com>
2016-11-25 09:18:13 +0000

    rpsEngine logic and unit tests

--- app/components/rpsEngine/rpsEngine_test.js
+++ app/components/rpsEngine/rpsEngine_test.js
@@ -18,6 +18,8 @@ describe("Directive: rpsEngine", function () {
     };
   }));
 
+  var badData = [{choice: "rock"}, ["rock"], 1, true, false];
+
   describe("Function: rpsChoose", function ()  {
     it("should be defined on load", function () {
 
@@ -26,6 +28,180 @@ describe("Directive: rpsEngine", function () {
 
       //Assert
       expect(scope.rpsChoose).toBeDefined();
+    });
+
+    it("should define compChoice", function() {
+
+      //Arrange
+      compile();
+
+      //Act
+      scope.rpsChoose();
+
+      //Assert
+      expect(scope.compChoice).toBeDefined()
+    })
+  });
+
+  describe("Function: compareChoices", function () {
+    it("should be defined on load", function () {
+
+      //Arrange
+      compile();
+
+      //Assert
+      expect(scope._compareChoices).toBeDefined()
+
+    });
+
+    it("should set result to undefined when bad data is passed to the function", function() {
+
+      //Arrange
+      compile();
+
+      for (var i =0; i <= badData.length; i++) {
+        //Act
+        scope._compareChoices(badData[i]);
+
+        //Assert
+        expect(scope.result).toBe(undefined);
+      }
+
+    });
+
+    it("should set result as draw when the choices are comp: rock, player: rock", function () {
+
+      //Arrange
+      compile();
+      scope.compChoice = "rock";
+      scope.playerChoice = "rock";
+
+      //Act
+      scope._compareChoices(scope.playerChoice);
+
+      //Assert
+      expect(scope.result).toBe("Draw");
+
+    });
+
+    it("should set result as draw when the choices are comp: paper, player: paper", function () {
+
+      //Arrange
+      compile();
+      scope.compChoice = "paper";
+      scope.playerChoice = "paper";
+
+      //Act
+      scope._compareChoices(scope.playerChoice);
+
+      //Assert
+      expect(scope.result).toBe("Draw");
+
+    });
+
+    it("should set result as draw when the choices are comp: scissors, player: scissors", function () {
+
+      //Arrange
+      compile();
+      scope.compChoice = "scissors";
+      scope.playerChoice = "scissors";
+
+      //Act
+      scope._compareChoices(scope.playerChoice);
+
+      //Assert
+      expect(scope.result).toBe("Draw");
+
+    });
+
+    it("should set result as player when the choices are comp: scissors, player: rock", function () {
+
+      //Arrange
+      compile();
+      scope.compChoice = "scissors";
+      scope.playerChoice = "rock";
+
+      //Act
+      scope._compareChoices(scope.playerChoice);
+
+      //Assert
+      expect(scope.result).toBe("Player");
+
+    });
+
+    it("should set result as comp when the choices are comp: paper, player: rock", function () {
+
+      //Arrange
+      compile();
+      scope.compChoice = "paper";
+      scope.playerChoice = "rock";
+
+      //Act
+      scope._compareChoices(scope.playerChoice);
+
+      //Assert
+      expect(scope.result).toBe("Comp");
+
+    });
+
+    it("should set result as player when the choices are comp: rock, player: paper", function () {
+
+      //Arrange
+      compile();
+      scope.compChoice = "rock";
+      scope.playerChoice = "paper";
+
+      //Act
+      scope._compareChoices(scope.playerChoice);
+
+      //Assert
+      expect(scope.result).toBe("Player");
+
+    });
+
+    it("should set result as comp when the choices are comp: scissors, player: paper", function () {
+
+      //Arrange
+      compile();
+      scope.compChoice = "scissors";
+      scope.playerChoice = "paper";
+
+      //Act
+      scope._compareChoices(scope.playerChoice);
+
+      //Assert
+      expect(scope.result).toBe("Comp");
+
+    });
+
+    it("should set result as player when the choices are comp: paper, player: scissors", function () {
+
+      //Arrange
+      compile();
+      scope.compChoice = "paper";
+      scope.playerChoice = "scissors";
+
+      //Act
+      scope._compareChoices(scope.playerChoice);
+
+      //Assert
+      expect(scope.result).toBe("Player");
+
+    });
+
+    it("should set result as comp when the choices are comp: rock, player: scissors", function () {
+
+      //Arrange
+      compile();
+      scope.compChoice = "rock";
+      scope.playerChoice = "scissors";
+
+      //Act
+      scope._compareChoices(scope.playerChoice);
+
+      //Assert
+      expect(scope.result).toBe("Comp");
+
     });
   });
 
